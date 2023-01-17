3972d7a2e7005dba0085d318f14cda69ca79fc74
Victor Lee <victor@logishub.net>
2023-01-17 10:54:37 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setDoTos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setDoTos((currentArray) => [toDo, ...currentArray]);
+    console.log(toDos);
+    setToDo("");
+  }
   return (
+    <Router>
     <div>
-      <h1>Welcome back React!</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange}
+          value={toDo}
+          type="text" 
+          placeholder="Write your to do..">
+          </input>
+        <button>Add to do</button>
+      </form>
     </div>
+    </Router>
   );
 }
 
